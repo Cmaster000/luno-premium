2893512e1b6555741b7bf172c21c2492489137c8
Caleb Alberson <calebalberson@Calebs-MacBook-Air.local>
2024-01-23 06:22:09 +0300

    added all current files

--- index.js
+++ index.js
@@ -1,49 +1,25 @@
-async function exchangeRateFunc() {
-  let myHeader = new Headers()
-  myHeader.append(process.env.YOUR_NAME, process.env.API_VALUE)
+import { binanceRateFunc } from './lib/binance.js'
+import { getLunoBTCPriceInMYR } from './lib/luno.js'
+import { exchangeRateFunc } from './lib/exchange-rate.js'
 
-  let requestOptions = {
-    method: 'GET',
-    redirect: 'follow',
-    headers: myHeader
-  };
-
-  const exResponse = await fetch("https://api.apilayer.com/exchangerates_data/convert?to=myr&from=usd&amount=1", requestOptions)
-  const exResults = await exResponse.json()
-  // console.log(exResults.result)
-  return Number(exResults.result)
-}
-
-async function lunoRateFunc() {
-
-
-  const lunoResponse = await fetch("https://api.luno.com/api/1/ticker?pair=XBTMYR")
-  const lunoResults = await lunoResponse.json()
-  // console.log(lunoResults.last_trade)
-  return Number(lunoResults.last_trade)
-}
-
-async function binanceRateFunc() {
-   let ticker = await binance.prices()
-    return +ticker.BTCBUSD
-  }
-
-async function calcAll() {
-let exRate = await exchangeRateFunc()
-let lunRate = await lunoRateFunc()
-let binRate = await binanceRateFunc()
-
-console.log(typeof(binRate))
-
-let lunoUSD = lunRate / exRate
-let diff = lunoUSD - binRate
-let lunoPremium = (diff/lunoUSD) * 100
-
-console.log('Price of Bitcoin in Luno - ' + lunoUSD)
-console.log('USDMYR - ' + exRate)
-console.log('Price of Bitcoin in Binance - ' + binRate)
-console.log('Difference between Luno and Binance - ' + diff)
-console.log('Luno premium - ' + lunoPremium)
-}
+// await binanceRateFunc()
+// await getLunoBTCPriceInMYR()
+// await exchangeRateFunc()
 
 calcAll()
+
+async function calcAll() {
+  let binRate = await binanceRateFunc()
+  let exRate = await exchangeRateFunc()
+  let lunRate = await getLunoBTCPriceInMYR()
+
+  let lunoUSD = lunRate / exRate
+  let diff = lunoUSD - binRate
+  let lunoPremium = (diff/lunoUSD) * 100
+
+  console.log('Price of Bitcoin in Luno - ' + lunoUSD)
+  console.log('USDMYR - ' + exRate)
+  console.log('Price of Bitcoin in Binance - ' + binRate)
+  console.log('Difference between Luno and Binance - ' + diff)
+  console.log('Luno premium - ' + lunoPremium + '%')
+}
